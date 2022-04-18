0b9ff536266250a8b32bc208df776f8e389dc083
Jeko <57463439+Jeko-Mus@users.noreply.github.com>
2022-04-18 17:11:13 +0200

    Update QR_code_detector.py

--- QR_code_detector.py
+++ QR_code_detector.py
@@ -7,7 +7,10 @@ qr_code_img = cv2.imread('img/qrcode.png')
 
 # convert from BGR to RGB
 rgb_qrcode = cv2.cvtColor(qr_code_img, cv2.COLOR_BGR2RGB)
-plt.imshow(rgb_qrcode)
+cv2.imshow('rgb_qrcode', rgb_qrcode)
+cv2.waitKey(0) 
+#closing all open windows 
+cv2.destroyAllWindows() 
 
 # detect where qr code is on the image
 detector = cv2.QRCodeDetector()
@@ -23,10 +26,18 @@ qrcode_copy = rgb_qrcode.copy()
 plt.figure(figsize = (10,8))
 cv2.rectangle(qrcode_copy, (372,241), (488,126), (0,0,255), 6)
 plt.imshow(qrcode_copy)
+cv2.imshow('qrcode', qrcode_copy)
+cv2.waitKey(0) 
+#closing all open windows 
+cv2.destroyAllWindows() 
 
 #crop the image to only show qrcode
 qrcode_cropped = qrcode_copy[126:241, 372:488]
 plt.imshow(qrcode_cropped)
+cv2.imshow('qrcode_cropped', qrcode_cropped)
+cv2.waitKey(0) 
+#closing all open windows 
+cv2.destroyAllWindows() 
 
 #save cropped image containing just the qr code
 cv2.imwrite('img/qrcode_cropped.png',qrcode_cropped)
